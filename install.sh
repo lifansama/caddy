4dfc683a5cae2f5fe927eda72cd200277df8895b
lifansama <lifansama@gmail.com>
2021-11-22 21:16:02 +0800

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+source /etc/profile
+
+apt update
+apt install zstd -y
+tag=$(wget -qO- -t1 -T2 "https://api.github.com/repos/lifansama/caddy/releases/latest" | grep "tag_name" | head -n 1 | awk -F ":" '{print $2}' | sed 's/\"//g;s/,//g;s/ //g')
+wget https://github.com/lifansama/caddy/releases/download/$tag/caddy.tar.zst
+tar xvf caddy.tar.zst
+
+chmod +x caddy
+mv caddy /usr/bin/
+mkdir /etc/caddy
+touch /etc/caddy/Caddyfile
+
+groupadd --system caddy
+useradd --system \
+    --gid caddy \
+    --create-home \
+    --home-dir /var/lib/caddy \
+    --shell /usr/sbin/nologin \
+    --comment "Caddy web server" \
+    caddy
+
+cat << EOF > /etc/systemd/system/caddy.service
+[Unit]
+Description=Caddy
+Documentation=https://caddyserver.com/docs/
+After=network.target network-online.target
+Requires=network-online.target
+
+[Service]
+User=caddy
+Group=caddy
+ExecStart=/usr/bin/caddy run --environ --config /etc/caddy/Caddyfile
+ExecReload=/usr/bin/caddy reload --config /etc/caddy/Caddyfile
+TimeoutStopSec=5s
+LimitNOFILE=1048576
+LimitNPROC=512
+PrivateTmp=true
+ProtectSystem=full
+AmbientCapabilities=CAP_NET_BIND_SERVICE
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+systemctl daemon-reload
+systemctl enable caddy
