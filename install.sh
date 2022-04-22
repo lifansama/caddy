1375ccb40b54871111184ef091fc5764e2da3331
lifansama <lifansama@gmail.com>
2022-04-22 04:03:14 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,8 +2,8 @@
 source /etc/profile
 
 tag=$(wget -qO- -t1 -T2 "https://api.github.com/repos/lifansama/caddy/releases/latest" | grep "tag_name" | head -n 1 | awk -F ":" '{print $2}' | sed 's/\"//g;s/,//g;s/ //g')
-wget https://github.com/lifansama/caddy/releases/download/$tag/caddy.tar.xz
-tar xvf caddy.tar.xz
+wget https://github.com/lifansama/caddy/releases/download/$tag/caddy_linux_amd64.tar.zst
+tar xvf caddy_linux_amd64.tar.zst
 
 chmod +x caddy
 mv caddy /usr/bin/
