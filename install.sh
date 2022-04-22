5037d480e9d14edfd81186f33122401eb82deeee
lifansama <lifansama@gmail.com>
2022-04-22 01:33:25 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 source /etc/profile
 
-apt update
-apt install zstd -y
 tag=$(wget -qO- -t1 -T2 "https://api.github.com/repos/lifansama/caddy/releases/latest" | grep "tag_name" | head -n 1 | awk -F ":" '{print $2}' | sed 's/\"//g;s/,//g;s/ //g')
-wget https://github.com/lifansama/caddy/releases/download/$tag/caddy.tar.zst
-tar xvf caddy.tar.zst
+wget https://github.com/lifansama/caddy/releases/download/$tag/caddy.tar.xz
+tar xvf caddy.tar.xz
 
 chmod +x caddy
 mv caddy /usr/bin/
 mkdir /etc/caddy
-rm -rf caddy.tar.zst
+rm -rf caddy.tar.xz
 
 groupadd --system caddy
 useradd --system \
