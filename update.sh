3cf16eb65c846b314bd8688a42c959e05c574b90
lifansama <lifansama@gmail.com>
2022-08-15 02:57:48 +0000

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 source /etc/profile
-rm -rf caddy_linux_amd64.tar.zst
-rm -rf caddy
-wget https://github.com/lifansama/caddy/releases/latest/download/caddy_linux_amd64.tar.zst
+cd /dev/shm
+rm -f caddy_linux_amd64.tar.zst
+rm -f caddy
+wget -q https://github.com/lifansama/caddy/releases/latest/download/caddy_linux_amd64.tar.zst
 tar xvf caddy_linux_amd64.tar.zst
-rm -rf /usr/bin/caddy
-mv caddy /usr/bin
-chmod +x /usr/bin/caddy
+mv -f caddy /usr/bin
 caddy version
+rm -f caddy_linux_amd64.tar.zst
 service caddy restart
-rm -rf caddy_linux_amd64.tar.zst
-rm -rf caddy
-
