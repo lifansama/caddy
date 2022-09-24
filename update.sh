3c5c17c661416aa3203be854711a74ecfb1663b9
lifansama <lifansama@gmail.com>
2022-09-24 02:56:15 +0000

    Update update.sh

--- update.sh
+++ update.sh
@@ -3,7 +3,7 @@ source /etc/profile
 cd /dev/shm
 rm -f caddy
 caddy version
-wget -q https://github.com/lifansama/caddy/releases/latest/download/caddy_linux_amd64.tar.zst -O - | tar -xvf
+wget -q https://github.com/lifansama/caddy/releases/latest/download/caddy_linux_amd64.tar.zst -O - | tar -xv
 mv -f caddy /usr/bin
 caddy version
 service caddy restart
